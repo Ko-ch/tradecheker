4dd8fc693b3e70614c9968827b9235ebafb3e93d
Koichiro <koichiro_takagi@outlook.jp>
2019-10-13 20:31:39 +0900

    指定のパスファイルから通貨情報を読み込みできるようになった

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-def main():
-    pass
+import os
 
+
+def main():
+    path_1 = os.path.join('C:\\Users', 'koich', 'OneDrive', 'デスクトップ', '04_app', '01_tradechecker', 'sampledata', )
+    os.chdir(path_1)
+
+    with open('EURUSD1.csv', mode='r', encoding='utf-8') as f:
+        text = f.read()
 
 if __name__ == "__main__":
     main()
